926e4b9e5217f5d7937e06064ebf1e252c2db2c5
vuxwindy <96514085+vuxwindy@users.noreply.github.com>
2025-09-18 15:26:25 +0700

    add team home

--- app/page.tsx
+++ app/page.tsx
@@ -1130,53 +1130,6 @@ export default function Home() {
                     </a>
                   </div>
                 </Marquee>
-
-                <section data-v-f64e8edb='' id='team' className='content-section padding-large bg-dark'>
-        <div data-v-f64e8edb='' className='container'>
-          <div data-v-f64e8edb='' className='row'>
-            <div data-v-f64e8edb='' className='col-12'>
-              <div data-v-f64e8edb='' className='content-card'>
-                <h2 data-v-f64e8edb='' className='content-title'>
-                  <i data-v-f64e8edb='' className='fas fa-users me-3' /> Team
-                </h2>
-                <div data-v-f64e8edb='' className='content-body'>
-                  <div data-v-f64e8edb='' className='team-grid'>
-                    {teams.map((team) => {
-                      return (
-                        <div key={team.id} data-v-f64e8edb='' className='team-member'>
-                          <div data-v-f64e8edb='' className='member-avatar'>
-                            <Image data-v-f64e8edb='' src={team.avatar} alt='Alex Chen' className='avatar-image' />
-                          </div>
-                          <h4 data-v-f64e8edb='' className='member-name'>
-                            {team.name}
-                          </h4>
-                          <p data-v-f64e8edb='' className='member-role'>
-                            {team.role}
-                          </p>
-                          <p data-v-f64e8edb='' className='member-bio'>
-                            {team.bio}
-                          </p>
-                          <div data-v-f64e8edb='' className='member-social'>
-                         {/* <Link data-v-f64e8edb='' href={team.linkedin} target='_blank' className='social-link'>
-                             <FaLinkedin />
-                            </Link> */}
-                             {team.id === 1 && team.twitter && (
-                            <Link data-v-f64e8edb='' href={team.twitter} target='_blank' className='social-link'>
-                              <FaTwitter /> 
-
-                            </Link>  
-                        )}
-                          </div> 
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
                 {/* <div className='grid grid-cols-5 gap-4'>
                   <div className='flex items-center justify-center'>
                     <Image
@@ -1251,6 +1204,53 @@ export default function Home() {
                 </div> */}
               </div>
             </section>
+  <section data-v-f64e8edb='' id='team' className='content-section padding-large bg-dark'>
+        <div data-v-f64e8edb='' className='container'>
+          <div data-v-f64e8edb='' className='row'>
+            <div data-v-f64e8edb='' className='col-12'>
+              <div data-v-f64e8edb='' className='content-card'>
+                <h2 data-v-f64e8edb='' className='content-title'>
+                  <i data-v-f64e8edb='' className='fas fa-users me-3' /> Team
+                </h2>
+                <div data-v-f64e8edb='' className='content-body'>
+                  <div data-v-f64e8edb='' className='team-grid'>
+                    {teams.map((team) => {
+                      return (
+                        <div key={team.id} data-v-f64e8edb='' className='team-member'>
+                          <div data-v-f64e8edb='' className='member-avatar'>
+                            <Image data-v-f64e8edb='' src={team.avatar} alt='Alex Chen' className='avatar-image' />
+                          </div>
+                          <h4 data-v-f64e8edb='' className='member-name'>
+                            {team.name}
+                          </h4>
+                          <p data-v-f64e8edb='' className='member-role'>
+                            {team.role}
+                          </p>
+                          <p data-v-f64e8edb='' className='member-bio'>
+                            {team.bio}
+                          </p>
+                          <div data-v-f64e8edb='' className='member-social'>
+                         {/* <Link data-v-f64e8edb='' href={team.linkedin} target='_blank' className='social-link'>
+                             <FaLinkedin />
+                            </Link> */}
+                             {team.id === 1 && team.twitter && (
+                            <Link data-v-f64e8edb='' href={team.twitter} target='_blank' className='social-link'>
+                              <FaTwitter /> 
+
+                            </Link>  
+                        )}
+                          </div> 
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+            
             {/* <ReferralComponent /> */}
 
             {/* <section className='nft-investment-section'>
